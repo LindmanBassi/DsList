d5ab19116217bb6ce9fed0472fd276bf24499144
hnrqb <hnrqbassi132@gmail.com>
2025-05-19 16:04:08 -0300

    Game seed

--- src/main/java/br/com/bassi/DsList/entities/Game.java
+++ src/main/java/br/com/bassi/DsList/entities/Game.java
@@ -16,13 +16,18 @@ public class Game {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name = "game_year")
+
     private String title;
+    @Column(name = "game_year")
     private Integer year;
+
     private String genre;
-    private String platform;
+    private String platforms;
+    private Double score;
     private String imgUrl;
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
 }
