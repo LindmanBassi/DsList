b32d46a14bf497f0ebe3f90a135c3c0dd4f3cbe3
hnrqb <hnrqbassi132@gmail.com>
2025-05-20 09:50:53 -0300

    Domain model

---
+++ src/main/java/br/com/bassi/DsList/domain/GameList.java
@@ -0,0 +1,18 @@
+package br.com.bassi.DsList.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+@Entity
+
+@Table(name = "tb_game_list")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+public class GameList {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
